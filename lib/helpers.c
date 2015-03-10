715cc2cd2fa102c8d77fb6898bc70e423e879383
Au-Rikka <anastasiyamaksimovskih@gmail.com>
2015-03-11 00:23:59 +0300

    Some changes in helpers.c Added makefile. I hope it works

--- lib/helpers.c
+++ lib/helpers.c
@@ -4,7 +4,7 @@ ssize_t read_(int fd, void* buf, size_t count) {
     ssize_t bytes_read = 0;
     ssize_t cur = 1;
 
-    while (cur > 0) {
+    while (cur > 0 && count > 0) {
         cur = read(fd, buf + bytes_read, count);
         if (cur < 0) {
             return cur;
@@ -21,7 +21,7 @@ ssize_t write_(int fd, void* buf, size_t count) {
     ssize_t bytes_write = 0;
     ssize_t cur = 1;
 
-    while (cur != 0) {
+    while (cur > 0 && count > 0) {
         cur = write(fd, buf + bytes_write, count);
         if (cur < 0) {
             return cur;
