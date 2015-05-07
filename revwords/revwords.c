317c82d8eb601f0cce114ad01b9f67fa2a6b8bc9
Au-Rikka <anastasiyamaksimovskih@gmail.com>
2015-05-07 17:18:54 +0300

    Revert "123"
    This reverts commit 1507177796b53ec7b990181c79b86c25334d8210.

--- revwords/revwords.c
+++ revwords/revwords.c
@@ -1,69 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "helpers.h"
 
 size_t const BUF_SIZE = 4097;
 
 int main() {
     char* buf = (char*) malloc(BUF_SIZE);
-    char* rev_buf = (char*) malloc(BUF_SIZE);
- 
-    if (buf == NULL || rev_buf == NULL) {
-        perror("malloc failed");
-        return EXIT_FAILURE;
-    }
-
-    ssize_t bytes_read = 0;
+    char rev_buf[BUF_SIZE];
+    ssize_t bytes_read = 1;
     ssize_t bytes_write = 0;
     int offset = 0;
-    int ri = 0;
 
-    while (1) {
+    while (bytes_read > 0) {
         bytes_read = read_until(STDIN_FILENO, buf + offset, BUF_SIZE, ' ');
-        
         if (bytes_read < 0) {
             perror("Input error");
             return EXIT_FAILURE;
         }
-        if (bytes_read == 0) {
-            break;
-        }
-        
-        int i, j;
-        int kol = 0;
-        for (i = offset; i < bytes_read + offset; i++) {
-            if (buf[i] == ' ') {
-                for (j = i - 1; j >= kol; j--) {
-                    rev_buf[ri] = buf[j];
-                    ri++;
+        if (bytes_read > 0) {
+            int i, j;
+            int st = 0;
+            for (i = offset; i < bytes_read + offset; i++) {
+                if (buf[i] == ' ') {
+                    for (j = i - 1; j >= st; j--) {
+                        rev_buf[i - 1 - j] = buf[j];
+                    }
+                    rev_buf[i - st] = ' ';
+                    bytes_write = write_(STDOUT_FILENO, rev_buf, i - st + 1);
+                    if (bytes_write < 0) {
+                        perror("Output error");
+                        return EXIT_FAILURE;
+                    }
+                    st = i + 1;
                 }
-
-                rev_buf[ri] = ' ';
-                ri++;
-
-                bytes_write = write_(STDOUT_FILENO, rev_buf, ri);
-                if (bytes_write < 0) {
-                    perror("Output error");
-                    return EXIT_FAILURE;
-                } 
-
-                ri -= bytes_write;
-                memmove(rev_buf, rev_buf + bytes_write, ri);
-                kol += bytes_write;
             }
+            offset += bytes_read - st;
+            buf += st;
         }
-
-        offset += bytes_read - kol;
-        memmove(buf, buf + kol, offset);
     }
 
     int j;
-    for (j = offset - 1; j >= 0; j--) {
-        rev_buf[ri] = buf[j];
-        ri++;
+    for (j = offset; j >= 0; j--) {
+        rev_buf[offset - j] = buf[j];
     }
-    bytes_write = write_(STDOUT_FILENO, rev_buf, ri);
+    bytes_write = write_(STDOUT_FILENO, rev_buf, offset + 1);
     if (bytes_write < 0) {
         perror("Output error");
         return EXIT_FAILURE;
